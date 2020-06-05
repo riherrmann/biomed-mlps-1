d427aafe6c06e2fc99af7f716ca6b3d71ddf30c3
Richard Herrmann <richard.herrmann@scout24.com>
2020-06-05 12:52:01 +0200

    open tsv data

--- biomed/file_handler.py
+++ biomed/file_handler.py
@@ -1,3 +1,12 @@
+import csv
+
+
 class FileHandler:
     def __init__(self):
         pass
+
+    def open_tsv(self, location):
+        tsv_file = open(location)
+        read_tsv = csv.reader(tsv_file, delimiter="\t")
+        tsv_file.close()
+        return read_tsv
--- biomed/main.py
+++ biomed/main.py
@@ -1,2 +1,7 @@
+from biomed import file_handler
+from biomed.file_handler import FileHandler
+
 if __name__ == '__main__':
-    print('test')
+    training_data_location = "training_data/train.tsv"
+    file_handler = FileHandler()
+    training_data = file_handler.open_tsv(training_data_location)
