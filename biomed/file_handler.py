6dde664dbfabb8b292dc557f95deeeac85db0b29
Richard Herrmann <richard.herrmann@scout24.com>
2020-06-05 13:04:42 +0200

    extend import

--- biomed/file_handler.py
+++ biomed/file_handler.py
@@ -5,8 +5,16 @@ class FileHandler:
     def __init__(self):
         pass
 
-    def open_tsv(self, location):
-        tsv_file = open(location)
-        read_tsv = csv.reader(tsv_file, delimiter="\t")
-        tsv_file.close()
+    def open_tsv_as_list(self, location):
+        output = []
+        with open(location) as tsv_file:
+            read_tsv = csv.reader(tsv_file, delimiter="\t")
+            for row in read_tsv:
+                output.append(row)
+        return output
+
+
+    def open_tsv_as_dict(self, location):
+        with open(location) as tsv_file:
+            read_tsv = csv.DictReader(tsv_file, delimiter="\t")
         return read_tsv
--- biomed/main.py
+++ biomed/main.py
@@ -4,4 +4,7 @@ from biomed.file_handler import FileHandler
 if __name__ == '__main__':
     training_data_location = "training_data/train.tsv"
     file_handler = FileHandler()
-    training_data = file_handler.open_tsv(training_data_location)
+    training_data = file_handler.open_tsv_as_list(training_data_location)
+    print(type(training_data))
+    for row in training_data[:2]:
+        print(row)
