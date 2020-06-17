8209579f035d9dc52e9d52ab636966aabaf919cb
Richard Herrmann <richard.herrmann@scout24.com>
2020-06-17 11:00:46 +0200

    add tfidf transformation

--- biomed/text_mining_manager.py
+++ biomed/text_mining_manager.py
@@ -1,3 +1,4 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
 
@@ -6,5 +7,15 @@ class TextMiningManager:
         pass
 
     def train_test_split(self, data):
-        training_data, test_data = train_test_split(data, test_size = 0.3)
+        training_data, test_data = train_test_split(data, test_size=0.3)
         return training_data, test_data
+
+    def tfidf_transformation(self, training_data, test_data, max_features=200000):
+        vectorizer = TfidfVectorizer(min_df=2, max_df=0.95, max_features=max_features, ngram_range=(1, 4),
+                                     sublinear_tf=True)
+
+        vectorizer = vectorizer.fit(training_data['text'])
+        training_features = vectorizer.transform(training_data['text'])
+
+        test_features = vectorizer.transform(test_data['text'])
+        return training_features, test_features
--- tests/test_text_mining_manager.py
+++ tests/test_text_mining_manager.py
@@ -1,12 +1,25 @@
 from biomed.file_handler import FileHandler
 from biomed.text_mining_manager import TextMiningManager
 
-def test_train_test_split():
+
+def test_train_test_split(datadir):
     fm = FileHandler()
-    data = fm.read_tsv_pandas_data_structure("../training_data/train.tsv")
+    data = fm.read_tsv_pandas_data_structure(datadir / "test_train.tsv")
     sut = TextMiningManager()
     training_data, test_data = sut.train_test_split(data)
     assert training_data.shape == (int(data.shape[0] * 0.7), 5)
     assert test_data.shape == (int(data.shape[0] * 0.3), 5) or test_data.shape == (int(data.shape[0] * 0.3) + 1, 5)
 
     # train, test = train_test_split(data, test_size = 0.3)
+
+
+def test_tfidf_transformation(datadir):
+    fm = FileHandler()
+    data = fm.read_tsv_pandas_data_structure(datadir / "test_train.tsv")
+    sut = TextMiningManager()
+    training_data, test_data = sut.train_test_split(data)
+    max_features = 10000
+    training_features, test_features = sut.tfidf_transformation(training_data, test_data, max_features)
+
+    assert training_features.shape[0] == int(training_data.shape[0]) and training_features.shape[0] <= max_features
+    assert test_features.shape[0] == int(test_data.shape[0]) and test_features.shape[0] <= max_features
