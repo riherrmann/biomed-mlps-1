ac258f680adb0ba41ef8307dc9b7998c4c5642a4
bitPogo <geislemx@informatik.hu-berlin.de>
2020-06-21 13:07:30 +0200

    Wire up Cache in Preprocessor
    This patch wires the Preprocessor and the SharedMemory Cache.
    However at the moment all Intances use the same Cache.

--- biomed/preprocessor/polymorph_preprocessor.py
+++ biomed/preprocessor/polymorph_preprocessor.py
@@ -4,36 +4,64 @@ from nltk import sent_tokenize
 from normalizer.normalizer import Normalizer
 from normalizer.simpleNormalizer import SimpleNormalizer
 from normalizer.complexNormalizer import ComplexNormalizer
+from cache.cache import Cache
+from cache.sharedMemoryCache import SharedMemoryCache
 from pandas import DataFrame
 
 class PolymorphPreprocessor( PreProcessor ):
     def __init__(
         self,
+        Cache: Cache,
         Simple: Normalizer,
         SimpleFlags: list,
         Complex: Normalizer,
         ComplexFlags: list
     ):
+        self.__Cache = Cache
         self.__SimpleFlags = SimpleFlags
         self.__Simple = Simple
         self.__ComplexFlags = ComplexFlags
         self.__Complex = Complex
 
     def preprocess_text_corpus( self, frame: DataFrame, flags: str ) -> list:
-        return self.__extractText( list( frame[ "text" ] ), flags )
+        return self.__extractText(
+            frame[ "pmid" ],
+            list( frame[ "text" ] ),
+            flags
+        )
 
-    def __extractText( self, Text: list, Flags: str ) -> list:
+    def __extractText( self, Pmid: list, Text: list, Flags: str ) -> list:
         for Index in range( 0, len( Text ) ):
-            Text[ Index ] = self.__reassemble(
-                self.__normalize(
-                    sent_tokenize(
-                        Text[ Index ]
-                    ),
-                    Flags
-                )
+            Text[ Index ] = self.__useCacheOrNormalizer(
+                Pmid[ Index ],
+                Text[ Index ],
+                Flags
             )
 
         return Text
+
+    def __useCacheOrNormalizer( self, Pmid: int, Text: str, Flags: str ) -> str:
+        Flags = self.__toSortedString( Flags )
+        CacheKey = "{}{}".format( Pmid, Flags )
+        if self.__Cache.has( CacheKey ):
+            return self.__Cache.get( CacheKey )
+        else:
+            return self.__applyTextNormalizerAndCache( CacheKey, Text, Flags )
+
+    def __toSortedString( self, Str: str ) -> str:
+        Tmp = list( Str )
+        Tmp.sort()
+        return "".join( Tmp )
+
+    def __applyTextNormalizerAndCache( self, CacheKey: str, Text: str, Flags: str ) -> str:
+        Result = self.__applyTextNormalizer( Text, Flags )
+        self.__Cache.set( CacheKey, Result )
+        return Result
+
+    def __applyTextNormalizer( self, Text: str, Flags: str ) -> str:
+        return self.__reassemble(
+            self.__normalize( sent_tokenize( Text ), Flags )
+        )
 
     def __normalize( self, Sentences: list, Flags: str ) -> list:
         ParsedSentences = list()
@@ -45,10 +73,10 @@ class PolymorphPreprocessor( PreProcessor ):
     def __normalizePerSentence( self, Text: str, Flags: str ) -> str:
         ParsedSentence = Text
         if self.__useComplex( Flags ):
-            ParsedSentence = self.__reassemble( self.__Complex.apply( ParsedSentence, Flags ) )
+            ParsedSentence = self.__Complex.apply( ParsedSentence, Flags )
 
         if self.__useSimple( Flags ):
-            ParsedSentence = self.__reassemble( self.__Simple.apply( ParsedSentence, Flags ) )
+            ParsedSentence = self.__Simple.apply( ParsedSentence, Flags )
 
         return ParsedSentence
 
@@ -78,10 +106,12 @@ class PolymorphPreprocessor( PreProcessor ):
         __SimpleFlags = [ "s", "l", "w" ]
         __Complex = ComplexNormalizer.Factory.getInstance()
         __ComplexFlags = [ "n", "v", "a" ]
+        __DistrubutedCache = SharedMemoryCache.Factory.getInstance()
 
         @staticmethod
         def getInstance() -> PreProcessor:
             return PolymorphPreprocessor(
+                PolymorphPreprocessor.Factory.__DistrubutedCache,
                 PolymorphPreprocessor.Factory.__Simple,
                 PolymorphPreprocessor.Factory.__SimpleFlags,
                 PolymorphPreprocessor.Factory.__Complex,
--- tests/test_preprocessing/polymorphPreprocessorSpec.py
+++ tests/test_preprocessing/polymorphPreprocessorSpec.py
@@ -6,9 +6,32 @@ if AdditionalPath not in Sys.path:
     Sys.path.append( AdditionalPath )
 
 import unittest
+from normalizer.normalizer import Normalizer
+from cache.cache import Cache
 from polymorph_preprocessor import PolymorphPreprocessor
 from pre_processor import PreProcessor
 from pandas import DataFrame
+
+class StubbedNormalizer( Normalizer ):
+    def __init__( self ):
+        self.WasCalled = False
+
+    def apply( self, Text: str, Flags: str ) -> list:
+        self.WasCalled = True
+        return Text
+
+class StubbedCache( Cache ):
+    def __init__( self, GivenCache: dict ):
+        self.__GivenCache = GivenCache
+
+    def has( self, Key: str ) -> bool:
+        return Key in self.__GivenCache
+
+    def get( self, Key: str ) -> str:
+        return self.__GivenCache[ Key ]
+
+    def set( self, Key: str, Value: str ):
+        self.__GivenCache[ Key ] = Value
 
 class PolymorphPreprocessorSpec( unittest.TestCase ):
     def it_is_a_PreProcessor( self ):
@@ -132,20 +155,67 @@ class PolymorphPreprocessorSpec( unittest.TestCase ):
         )
 
     def it_uses_a_cache_to_determine_if_the_value_was_already_processed( self ):
-        pass
+        TestData = {
+            'pmid': [ 42 ],
+            'cancer_type': [ -1 ],
+            'doid': [ 23 ],
+            'is_cancer': [ False ],
+            'text': [ "Liquid chromatography with tandem mass spectrometry method for the simultaneous determination of multiple sweet mogrosides in the fruits of Siraitia grosvenorii and its marketed sweeteners. A high-performance liquid chromatography with electrospray ionization tandem mass spectrometry method has been developed and validated for the simultaneous quantification of eight major sweet mogrosides in different batches of the fruits of Siraitia grosvenorii and its marketed sweeteners." ],
+        }
 
+        FakeCache = { "42a":  TestData[ "text"][ 0 ].lower() }
+        Complex = StubbedNormalizer()
+        Simple = StubbedNormalizer()
+        SimpleFlags = [ "s", "l", "w" ]
+        ComplexFlags = [ "n", "v", "a" ]
+        Cache = StubbedCache( FakeCache )
 
-Suite = unittest.TestSuite()
-Suite.addTests( [
-    PolymorphPreprocessorSpec( "it_is_a_PreProcessor" ),
-    PolymorphPreprocessorSpec( "it_does_not_alter_the_source" ),
-    PolymorphPreprocessorSpec( "it_ignores_unknown_flags" ),
-    PolymorphPreprocessorSpec( "it_uses_simple_normalizer" ),
-    PolymorphPreprocessorSpec( "it_uses_complex_normalizer" ),
-    PolymorphPreprocessorSpec( "it_uses_both_normalizers" ),
-    PolymorphPreprocessorSpec( "it_iterates_over_all_given_texts" ),
-] )
+        MyPrepro = PolymorphPreprocessor(
+            Cache,
+            Simple,
+            SimpleFlags,
+            Complex,
+            ComplexFlags
+        )
 
+        MyFrame = DataFrame( TestData, columns = [ 'pmid', 'cancer_type', 'doid', 'is_cancer', 'text' ] )
+        Result = MyPrepro.preprocess_text_corpus( MyFrame, "a" )
+        self.assertFalse( Complex.WasCalled )
+        self.assertFalse( Simple.WasCalled )
+        self.assertEqual(
+             Result[ 0 ],
+             FakeCache[ "42a" ]
+         )
 
-Runner = unittest.TextTestRunner()
-Runner.run( Suite )
+    def it_caches_new_text_variants( self ):
+        TestData = {
+            'pmid': [ 42 ],
+            'cancer_type': [ -1 ],
+            'doid': [ 23 ],
+            'is_cancer': [ False ],
+            'text': [ "Liquid chromatography with tandem mass spectrometry method for the simultaneous determination of multiple sweet mogrosides in the fruits of Siraitia grosvenorii and its marketed sweeteners. A high-performance liquid chromatography with electrospray ionization tandem mass spectrometry method has been developed and validated for the simultaneous quantification of eight major sweet mogrosides in different batches of the fruits of Siraitia grosvenorii and its marketed sweeteners." ],
+        }
+
+        FakeCache = {}
+        Complex = StubbedNormalizer()
+        Simple = StubbedNormalizer()
+        SimpleFlags = [ "s", "l", "w" ]
+        ComplexFlags = [ "n", "v", "a" ]
+        Cache = StubbedCache( FakeCache )
+
+        MyPrepro = PolymorphPreprocessor(
+            Cache,
+            Simple,
+            SimpleFlags,
+            Complex,
+            ComplexFlags
+        )
+
+        MyFrame = DataFrame( TestData, columns = [ 'pmid', 'cancer_type', 'doid', 'is_cancer', 'text' ] )
+        MyPrepro.preprocess_text_corpus( MyFrame, "a" )
+        self.assertTrue( Complex.WasCalled )
+        self.assertTrue( "42a" in FakeCache )
+        self.assertEqual(
+            TestData[ "text" ][ 0 ],
+            FakeCache[ "42a" ]
+        )
