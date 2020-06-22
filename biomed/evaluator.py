3d69cb6fc4e164194ffbd173f6e3b27e0481d96a
christophfritze <christoph.fritze@zattoo.com>
2020-06-22 19:07:51 +0200

    add evaluator class and test

--- biomed/evaluator.py
+++ biomed/evaluator.py
@@ -1,10 +1,5 @@
 from sklearn.metrics import f1_score
 
 class Evaluator:
-    def __init__(self, y_true, y_pred, average):
-        self.y_true = y_true
-        self.y_pred = y_pred
-        self.average = average
-
-    def calc_f1_score(self):
-        return f1_score(self.y_true, self.y_pred, self.average)
+    def calc_f1_score(self, y_true, y_pred, average):
+        return f1_score(y_true, y_pred, average=average)
