d530451629293d0d11c29301139cbab5304a5478
bitPogo <geislemx@informatik.hu-berlin.de>
2020-06-24 13:08:29 +0200

    Ajdust TextMiner
    This patch adjust the TextMiner for futher use in the pipeline runner
    regaring sequential and non sequential output.

--- biomed/main.py
+++ biomed/main.py
@@ -1,6 +1,7 @@
+import collections
+
 from biomed.file_handler import FileHandler
 from biomed.pipeline_runner import PipelineRunner
-
 
 
 if __name__ == '__main__':
@@ -11,8 +12,13 @@ if __name__ == '__main__':
     Runner = PipelineRunner.Factory.getInstance( "is_cancer" )
     preds = Runner.run( [ { "id": 1, "data": training_data } ] )
 
-    print(preds)
-    #cancer_types_found = [x for x in preds if x != 0]
-    #cancer_types_found = tmm.map_doid_values_to_nonsequential(cancer_types_found)
-    #print('number of cancer predictions found:', len(cancer_types_found))
-    #print('(doid, count):', counter.most_common())
+    print(preds[ "1" ][ 0 ])
+
+    found = list()
+    for index in range( 0, len( preds[ "1" ][ 0 ] ) ):
+        if preds[ "1" ][ 0 ][ index ] != 0:
+            found.append( preds[ "1" ][ 1 ][ index ] )
+
+    print('number of cancer predictions found:', len(found))
+    counter = collections.Counter(found)
+    print('(doid, count):', counter.most_common())
