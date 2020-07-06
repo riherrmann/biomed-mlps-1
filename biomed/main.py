d49b754c2f71eef97dde82b84d553666da146be9
Richard Herrmann <richard.herrmann@scout24.com>
2020-07-06 19:37:57 +0200

    print results to csv

--- biomed/main.py
+++ biomed/main.py
@@ -1,6 +1,9 @@
 import collections
 import os as OS
 import sys as Sys
+from datetime import datetime
+
+from biomed.properties_manager import PropertiesManager
 
 AdditionalPath = OS.path.abspath( OS.path.join( OS.path.dirname( __file__ ), '..' ) )
 if AdditionalPath not in Sys.path:
@@ -12,9 +15,11 @@ from biomed.plotter import Plotter
 
 def printResults( Predictions ):
     def outputResults( prediction: list ):
+        output_predictions = f"pmid,{ PropertiesManager().classifier }\n"
         found_targets = list()
         found_pmids = list()
         for index in range( 0, len( prediction[ 0 ] ) ):
+            output_predictions += f"{prediction[ 3 ][ 'pmid' ].iloc[ index ]},{prediction[ 1 ][ index ]}\n"
             if prediction[ 0 ][ index ] != 0:
                 found_targets.append( prediction[ 1 ][ index ] )
                 found_pmids.append( prediction[ 3 ][ 'pmid' ].iloc[ index ] )
@@ -26,10 +31,19 @@ def printResults( Predictions ):
         counter = collections.Counter(found_targets)
         print('(doid, count):', counter.most_common())
         print('cancer found in articles with PMID:', found_pmids)
+        return output_predictions
 
     for key in Predictions:
         print( "Configuration ID ", key )
-        outputResults( Predictions[ key ] )
+        output_predictions = outputResults( Predictions[ key ] )
+        path = OS.path.abspath(
+            OS.path.join(
+                OS.path.dirname( __file__ ), "..", "results",
+                f"{ datetime.now().strftime('%Y-%m-%d_%H-%M-%S') }_{ key }.csv "
+            )
+        )
+        with open(path, "w") as file:
+            file.write(output_predictions)
 
 if __name__ == '__main__':
     training_data_location = training_data_location = OS.path.abspath(
