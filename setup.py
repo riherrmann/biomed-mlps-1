96bd3842638520b78c18f8be86e05773d4e900c3
bitPogo <geislemx@informatik.hu-berlin.de>
2020-07-14 17:48:23 +0200

    Gruenau patches
    This patch makes adjustments at the java client for Gruenau1-6. However,
    this destroys the current testsetup. It is highly recommanded to use the
    orginial setup for any futher development or fix the resulting issues.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ if not OS.path.isdir( "./.cache" ):
     OS.mkdir( "./.cache", 0o770 )
 
 if not OS.path.isfile( "./nlpclient/client.jar" ):
-    subprocess.run( [ "./nlpclient/gradlew", "build" ], capture_output = True )
+    subprocess.run( [ "./nlpclient/gradlew", "build" ] )
     subprocess.run ( [ "mv", "./nlpclient/build/libs/nlpclient-1.0.0.jar", "./nlpclient/client.jar" ] )
